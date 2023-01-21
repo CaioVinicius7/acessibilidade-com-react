443a85f9b9d9db9b3d79c7f9dac2d774e6bff955
Caio Vinícius <caio1525pereira@gmail.com>
2023-01-21 18:59:29 -0300

    feat: :tada: Base do projeto e configurações iniciais
    Acessibilidade na prática - Setup do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Acessibilidade no React</title>
+      </Head>
+
+      <main>
+        <h1> Acessibilidade no React </h1>
+      </main>
+    </>
+  );
+}
