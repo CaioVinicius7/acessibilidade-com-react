ee772909046cbb96810491b4a5c297740a8c17b0
Caio Vinícius <caio1525pereira@gmail.com>
2023-01-24 23:29:57 -0300

    feat: :wheelchair: Testando acessibilidade da página utilizando Axe
    Acessibilidade na prática - Testando acessibilidade da página

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+
+import { axeAccessibilityReporter } from "@/utils/axeAccessibilityReporter";
 
 import "@/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    axeAccessibilityReporter();
+  }, []);
+
   return <Component {...pageProps} />;
 }
